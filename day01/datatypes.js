1efd1c4db96b1de3bffd13d516e4f7b88f36b122
chayanthithan <kumarchayan31@gmail.com>
2024-03-29 00:24:21 +0530

    lecture materials
    Signed-off-by: chayanthithan <kumarchayan31@gmail.com>

--- day01/datatypes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let fname = "chayankumar";
-fname = "kumar";
-//fname=20;    //typescript does not allow this but javascript does it
-console.log(fname);
-console.log(fname);
--- day01/datatypes.ts
+++
@@ -1,5 +0,0 @@
-let fname="chayankumar";
-fname="kumar";
-//fname=20;    //typescript does not allow this but javascript does it
-console.log(fname)
-console.log(fname)
